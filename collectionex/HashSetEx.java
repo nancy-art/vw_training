511f75c8f02a9f848a8acf5ef2ffc40736eb1104
nancy-art <61631998+nancy-art@users.noreply.github.com>
2022-11-02 10:51:03 +0530

    Add files via upload

---
+++ collectionex/HashSetEx.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package collectionex;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author NANCY
+ *
+ */
+public class HashSetEx {
+	public static void main(String[] args) {
+	Set<Employee> st=new HashSet<Employee>();
+	  Employee e1 = new Employee(2,"Raj Malhotra",25000,10);
+		
+			st.add(e1);
+			
+	        Employee e = new Employee(1,"Anubhav Sinha",15000,10);
+	        
+	        st.add(e);
+	        
+	        st.add(new Employee(3,"Sangeeta Shah",35000,20));
+	        st.add(new Employee(2,"Raj Malhotra",25000,10));
+	        
+	        
+	        System.out.println(st);
+	        System.out.println(st.size());
+	        
+	}
+}
